1fe432b73578a3fae993fc0a7df66ae536248b7d
Mahesh Lee <maheshlee1234@gmail.com>
2023-04-16 18:48:06 +0530

    pause funtionality is half added

--- SoundBar/SoundBar.js
+++ SoundBar/SoundBar.js
@@ -2,7 +2,17 @@ const btns = document.querySelectorAll("button");
 
 btns.forEach((btn) => btn.addEventListener("click", playsound));
 
+let text = "applause";
+let prev;
 function playsound() {
   let audio = new Audio(`sounds/${this.innerText}.mp3`);
+  if (text != this.innerText) {
+    prev.pause();
+    console.log(prev, text);
+    text = this.innerText;
+    prev = audio;
+    prev.currentTime = 0;
+  }
+  prev = audio;
   audio.play();
 }
